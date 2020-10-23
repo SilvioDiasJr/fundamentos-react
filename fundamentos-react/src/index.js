4f7329bd7d38a6544f98ec0b18339fce9ec4f64f
Silvio <silvio.dias@etec.sp.gov.br>
2020-10-23 00:12:40 -0300

    adicionar import de ComParametro

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -2,11 +2,16 @@ import React from 'react'
 import ReactDom from 'react-dom'
 
 import Primeiro from './components/basicos/Primeiro'
+import ComParametro from './components/basicos/ComParametro'
 import './index.css'
 
 ReactDom.render(
   <div>
     <Primeiro />
+    <ComParametro 
+      titulo= "Situação do Aluno" 
+      aluno="Silvio" nota={7} 
+    />
   </div>,
   document.getElementById('root')
 )
