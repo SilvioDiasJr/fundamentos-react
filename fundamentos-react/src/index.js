15b787c808baf154899598f12891f24eb31eb649
Silvio <silvio.dias@etec.sp.gov.br>
2020-10-23 00:23:00 -0300

    criar import do Fragmento.jsx

--- fundamentos-react/src/index.js
+++ fundamentos-react/src/index.js
@@ -3,6 +3,7 @@ import ReactDom from 'react-dom'
 
 import Primeiro from './components/basicos/Primeiro'
 import ComParametro from './components/basicos/ComParametro'
+import Fragmento from './components/basicos/Fragmento'
 import './index.css'
 
 ReactDom.render(
@@ -12,6 +13,7 @@ ReactDom.render(
       titulo= "Situação do Aluno" 
       aluno="Silvio" nota={7} 
     />
+    <Fragmento />
   </div>,
   document.getElementById('root')
 )
