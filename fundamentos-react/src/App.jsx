c1d2c02045e23788d59825036e0c71e56a10b1b6
Silvio <silvio.dias@etec.sp.gov.br>
2020-10-24 01:26:11 -0300

    modificar passando parâmetro de cor para cada Card.jsx

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -14,25 +14,25 @@ const App = () => {
       <h1>Fundamentos React</h1>
       
       <div className="Cards">
-        <Card titulo="#04 - Desafio Aleatório">
+        <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
           <Aleatorio
             min={1}
             max={150}
           />
         </Card>
 
-        <Card titulo="#03 - Fragmento">
+        <Card titulo="#03 - Fragmento" color="#E94C6F">
           <Fragmento /> 
         </Card>
         
-        <Card titulo="#02 - Com Parâmetro">
+        <Card titulo="#02 - Com Parâmetro" color="#E8B71A">
           <ComParametro 
             titulo= "Situação do Aluno" 
             aluno="Silvio" nota={7} 
           /> 
         </Card>
         
-        <Card titulo="#01 - Primeiro Componente">
+        <Card titulo="#01 - Primeiro Componente" color="#588C73">
           <Primeiro />
         </Card>
       </div>
