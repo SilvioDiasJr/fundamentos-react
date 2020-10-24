6d3a729ded60f533d4767f017d837a0da2d11ea8
Silvio <silvio.dias@etec.sp.gov.br>
2020-10-23 21:29:52 -0300

    adicionar  importe do componente Aleatorio.jsx

--- fundamentos-react/src/components/basicos/App.jsx
+++ fundamentos-react/src/components/basicos/App.jsx
@@ -3,11 +3,16 @@ import React from 'react'
 import Primeiro from './Primeiro'
 import ComParametro from './ComParametro'
 import Fragmento from './Fragmento'
+import Aleatorio from './Aleatorio'
 
 const App = () => {
   return (
     <div id="app">
       <h1>Fundamentos React</h1>
+      <Aleatorio
+        min={1}
+        max={150}
+      />
       <Fragmento /> 
       <ComParametro 
         titulo= "Situação do Aluno" 
