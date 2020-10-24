36fef648b2f97a9f75509bd27e8f5df65bab8758
Silvio <silvio.dias@etec.sp.gov.br>
2020-10-24 02:13:17 -0300

    modificar parametro passado para componente filho

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -5,9 +5,10 @@ import ComParametro from './components/basicos/ComParametro'
 import Fragmento from './components/basicos/Fragmento'
 import Aleatorio from './components/basicos/Aleatorio'
 import Card from './components/basicos/layout/Card'
+import Familia from './components/basicos/Familia'
+import FamiliaMembro from './components/basicos/FamiliaMembro'
 
 import './App.css'
-import Familia from './components/basicos/Familia'
 
 const App = () => {
   return (
@@ -16,7 +17,11 @@ const App = () => {
       
       <div className="Cards">
         <Card titulo="#05 - Componente com Filhos" color="#00C8F8">
-          <Familia sobrenome="Ferreira"/>
+          <Familia sobrenome="Silva">
+            <FamiliaMembro nome="Pedro" />
+            <FamiliaMembro nome="Ana" />
+            <FamiliaMembro nome="Gustavo" />           
+          </Familia>
         </Card>
 
         <Card titulo="#04 - Desafio Aleatório" color="#FA6900">
