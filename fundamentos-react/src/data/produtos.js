8c8586b30963f9b7dc6c4bae89da004bb34097c7
Silvio <silvio.dias@etec.sp.gov.br>
2020-10-24 21:00:02 -0300

    criar lista de produtos

---
+++ fundamentos-react/src/data/produtos.js
@@ -0,0 +1,11 @@
+export default [
+  {id:1, nome: 'Teclado', preco: '80.00'},
+  {id:2, nome: 'Mouse', preco: '120.00'},
+  {id:3, nome: 'Monitor', preco: '500.00'},
+  {id:4, nome: 'Cabo de rede', preco: '30.00'},
+  {id:5, nome: 'Cabo HDMI', preco: '50.00'},
+  {id:6, nome: 'Notebook', preco: '2500.00'},
+  {id:7, nome: 'Placa de rede', preco: '600.00'},
+  {id:8, nome: 'Placa de video', preco: '800.00'},
+  {id:9, nome: 'Fonte de alimentação', preco: '90.00'},
+]
