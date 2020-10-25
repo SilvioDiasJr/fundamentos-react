03402924dd2c73f7ae8a7c7ac15505e0d533720c
Silvio <silvio.dias@etec.sp.gov.br>
2020-10-25 01:34:14 -0300

    modificar impotação dos componentes

--- fundamentos-react/src/App.jsx
+++ fundamentos-react/src/App.jsx
@@ -11,6 +11,8 @@ import ListaAlunos from './components/repeticao/ListaAlunos'
 import TabelaProdutos from './components/repeticao/TabelaProdutos'
 import ParOuImpar from './components/condicional/ParOuImpar'
 import UsuarioInfo from './components/condicional/UsuarioInfo'
+import DiretaPai from './components/comunicacao/DiretaPai'
+import IndiretaPai from './components/comunicacao/IndiretaPai'
 
 import './App.css'
 
@@ -20,6 +22,14 @@ const App = () => {
       <h1>Fundamentos React</h1>
       
       <div className="Cards">
+        <Card titulo="#10 - Comunicação Indireta" color="#8BAD39">
+          <IndiretaPai />
+        </Card>
+
+        <Card titulo="#09 - Comunicação Direta" color="#59323C">
+          <DiretaPai />
+        </Card>
+
         <Card titulo="#08 - Renderização Condicional" color="#982395">
           <ParOuImpar numero={21}/>
           <UsuarioInfo usuario={{nome: 'Fernando'}}/>
